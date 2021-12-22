6f8aea1261fc3e611438d97af314fb9ef4232563
trinhhoangyen <trinhhoangyen@192.168.1.17>
2021-12-22 16:49:53 +0700

    test

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7b513105-21e2-443c-a618-47310fb3b31c</testSuiteGuid>
+</TestSuiteEntity>
